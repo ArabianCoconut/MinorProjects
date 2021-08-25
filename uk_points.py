76cd6695c521d725108f9443b0386eabad225694
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2021-08-25 18:33:38 +0530

    Signed-off-by: ArabianCoconut <maheircool25@gmail.com>

---
+++ uk_points.py
@@ -0,0 +1,7 @@
+#This Script Calculates the total Points for Uk-Imigration System.
+# Reference: https://shorturl.gg/HZxpC
+# By ArabianCoconut
+
+import module_main
+
+print(module_main.data_fn1())
